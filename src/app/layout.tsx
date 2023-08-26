1e0c776fd0965c6a4f2f6d5c234d65ab1c58a4a2
MuaazNasir <mohidmuaaz120@gmail.com>
2023-08-26 23:16:26 +0500

    cube-mart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Cube Mart</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
